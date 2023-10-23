b5afb3fe03b6c70c4f9c56ca92472d6425744617
JuaniEscobar <Juanescobar97i@gmail.com>
2023-10-23 01:10:09 -0300

    second commit

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -6,7 +6,28 @@ module.exports = (sequelize) => {
   sequelize.define('dog', {
     name: {
       type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    weight: {
+      type: DataTypes.STRING,
       allowNull: false,
     },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
   });
-};
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,23 @@
 const { Router } = require('express');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+router.get('/dogs', (req, res) =>{
+        res.status(200).send("Arreglo de objetos donde cada uno es una raza")
+});
+router.get('/dogs/:idRaza', (req, res) =>{
+        res.status(200).send("Objeto con la raza que coincida con el id")
+});
+router.get('/dogs/name', (req, res) => {
+    const { name } = req.query;
+    res.status(200).send(`Razas de perro que coinciden con el nombre ${name}`);
+});
+
+module.exports = router;
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
